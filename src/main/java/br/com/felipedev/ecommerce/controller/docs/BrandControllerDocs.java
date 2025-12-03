14dc3300eade3f8ded8a52080d7d778fc8f108e3
Felipe-PereiraDev <felipe.pereirasilvaa@outlook.com>
2025-12-03 02:56:19 -0400

    feat(docs): criar interface de documentação para BrandController

---
+++ src/main/java/br/com/felipedev/ecommerce/controller/docs/BrandControllerDocs.java
@@ -0,0 +1,25 @@
+package br.com.felipedev.ecommerce.controller.docs;
+
+import br.com.felipedev.ecommerce.dto.brand.BrandRequestDTO;
+import br.com.felipedev.ecommerce.dto.brand.BrandResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Tag(name = "Brand", description = "Endpoints para gerenciar marcas")
+public interface BrandControllerDocs {
+
+    @Operation(summary = "Criar Marca", description = "Cria uma nova marca")
+    public ResponseEntity<BrandResponseDTO> createBrand(BrandRequestDTO request);
+
+    @Operation(summary = "Listar Marcas", description = "Retorna uma lista de marcas")
+    public ResponseEntity<List<BrandResponseDTO>> findAll();
+
+    @Operation(summary = "Atualizar Descrição", description = "Atualiza a descrição de uma marca existente")
+    public ResponseEntity<BrandResponseDTO> updateDescription(Long id, BrandRequestDTO request);
+    
+    @Operation(summary = "Deletar Marcar", description = "Deleta uma merca pelo seu id")
+    public ResponseEntity<BrandResponseDTO> deleteById(Long id);
+}
