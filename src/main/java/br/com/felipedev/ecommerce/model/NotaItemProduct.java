2fa04fa09648e5b2a316a8960867a43ae5936f0b
Felipe-PereiraDev <felipe.pereirasilvaa@outlook.com>
2025-12-03 02:53:46 -0400

    feat: criar entidade NotaItemProduct com campos, relacionamentos e mapeamentos JPA

---
+++ src/main/java/br/com/felipedev/ecommerce/model/NotaItemProduct.java
@@ -0,0 +1,49 @@
+package br.com.felipedev.ecommerce.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "nota_item_product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotaItemProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false, foreignKey = @ForeignKey(name = "product_fk", value = ConstraintMode.CONSTRAINT))
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "nota_fiscal_compra_id", nullable = false, foreignKey = @ForeignKey(name = "nota_fiscal_compra_fk", value = ConstraintMode.CONSTRAINT))
+    private NotaFiscalCompra notaFiscalCompra;
+
+    @Column(nullable = false)
+    private Double quantity;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof NotaItemProduct that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
