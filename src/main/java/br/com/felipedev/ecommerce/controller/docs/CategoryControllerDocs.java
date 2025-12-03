ec0aa60eeb79dcdf9f29b42430745650f6cc4d31
Felipe-PereiraDev <felipe.pereirasilvaa@outlook.com>
2025-12-03 02:56:37 -0400

    feat(docs): criar interface de documentação para CategoryController

---
+++ src/main/java/br/com/felipedev/ecommerce/controller/docs/CategoryControllerDocs.java
@@ -0,0 +1,25 @@
+package br.com.felipedev.ecommerce.controller.docs;
+
+import br.com.felipedev.ecommerce.dto.category.CategoryRequestDTO;
+import br.com.felipedev.ecommerce.dto.category.CategoryResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Tag(name = "Category", description = "Endpoints para gerenciar categorias")
+public interface CategoryControllerDocs {
+
+    @Operation(summary = "Criar Categoria", description = "Cria uma nova categoria")
+    public ResponseEntity<CategoryResponseDTO> createCategory(CategoryRequestDTO request);
+
+    @Operation(summary = "Listar Categorias", description = "Retorna uma lista de categorias")
+    public ResponseEntity<List<CategoryResponseDTO>> findAll();
+
+    @Operation(summary = "Atualizar Descrição", description = "Atualiza a descrição de uma categoria existente")
+    public ResponseEntity<CategoryResponseDTO> updateDescription(Long id, CategoryRequestDTO request);
+    
+    @Operation(summary = "Deletar Categoria", description = "Deleta uma merca pelo seu id")
+    public ResponseEntity<CategoryResponseDTO> deleteById(Long id);
+}
